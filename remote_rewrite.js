bc3d5993adc0bc8195e2142f4078b29a2e2241d9
Pannio <1821299140@qq.com>
2021-05-09 18:48:54 +0800

    Update remote_rewrite.js

--- remote_rewrite.js
+++ remote_rewrite.js
@@ -1,4 +1,4 @@
 hostname = www.wnflb2020.com
 
 // 福利吧
-https:\/\/www.wnflb2020.com\/apicloud\/?mod=getcheckin url script-request-header https://raw.githubusercontent.com/hlf2016/scripts/main/fuliba.js
+https:\/\/www\.wnflb2020\.com\/apicloud\/\?mod=getcheckin url script-request-header https://raw.githubusercontent.com/hlf2016/scripts/main/fuliba.js
