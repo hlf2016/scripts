cdf709b22bae6f02b5fc0866dd90bfdc788e931e
Pannio <1821299140@qq.com>
2021-05-17 16:33:30 +0800

    增加京东金融10豆豆

--- remote_rewrite.js
+++ remote_rewrite.js
@@ -1,4 +1,8 @@
-hostname = www.wnflb2020.com
+hostname = www.wnflb2020.com, ms.jr.jd.com
 
 // 福利吧
 https:\/\/www\.wnflb2020\.com\/apicloud\/\?mod=getcheckin url script-request-header https://raw.githubusercontent.com/hlf2016/scripts/main/fuliba.js
+
+// 京东金融 10 京豆
+^https?:\/\/ms\.jr\.jd\.com\/gw\/generic\/uc\/newna\/m\/userstat  url script-request-body https://raw.githubusercontent.com/dompling/Script/master/jd/jd_jr_cookie.js
+
